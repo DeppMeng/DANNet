1c24ecb8f2685de9e5ac86dfbac6ec0569d1b80d
Depu Meng <t-depumeng@microsoft.com>
2021-05-12 17:19:43 +0800

    added train_distributed

--- configs/train_config_distributed.py
+++ configs/train_config_distributed.py
@@ -84,5 +84,7 @@ def get_arguments():
     parser.add_argument('--local_rank', default=0, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
 
     return parser.parse_args()
